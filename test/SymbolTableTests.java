5c07cd84dfd361b0b2a583d2b9d445a5e0af992a
Tyler Bobik <shed412@gmail.com>
2017-11-26 23:29:20 -0500

    did sameValueDifferentKey test

--- test/SymbolTableTests.java
+++ test/SymbolTableTests.java
@@ -2,6 +2,7 @@
 import org.junit.Test;
 import static org.junit.Assert.*;
 import org.mockito.*;
+import java.math.BigInteger;
 
 import rpnpackage.SymbolTable;
 
@@ -15,8 +16,9 @@ public class SymbolTableTests {
 	public void exceptionPutTest() {
 
 		SymbolTable st = new SymbolTable();
-		
-		try{
+
+		try {
+
 			st.put("a", 99);
 			st.put("b", 91);
 			st.put("a", 13);
@@ -26,6 +28,33 @@ public class SymbolTableTests {
 		}
 
 		fail();
+	}
+
+	/**
+	 * This test will check that two different keys with
+	 * the same value are stored correctly
+	*/
+	@Test
+	public void sameValueDifferentKey() {
+
+		SymbolTable st = new SymbolTable();
+
+		String aKey = "a";
+		String bKey = "b";
+		
+		try {
+
+			st.put(aKey, 5);
+			st.put(bKey, 5);
+
+		} catch (Exception e) {
+			fail();
+		}
+
+		BigInteger aValue = st.get(aKey);
+		BigInteger bValue = st.get(bKey);
+
+		assertEquals(aValue, bValue);
 	}
 
 }
