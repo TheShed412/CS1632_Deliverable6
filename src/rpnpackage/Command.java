c4fd15d8f3beabfcfb87d44684c23941a38e574a
Tyler Bobik <shed412@gmail.com>
2017-11-27 03:59:55 -0500

    made command an interface

--- src/rpnpackage/Command.java
+++ src/rpnpackage/Command.java
@@ -2,22 +2,19 @@ package rpnpackage;
 
 /**
  * 
- * This class will represent one of 3 commands:
+ * This interface will represent one of 3 commands:
  * 	PRINT
  * 	LET
  * 	QUIT
  * 
 */
 
-public class Command {
+public interface Command {
 
 	public static final String PRINT = "PRINT";
 	public static final String QUIT = "QUIT";
 	public static final String LET = "LET";
 
-	private String command;
-	private String[] equation;
-	private char var;
-
+	public int evaluate(SymbolTable st);
 
 }
