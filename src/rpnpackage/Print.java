1888482a0ad526feb9e1b1f555de1d68a94d25f3
Tyler Bobik <shed412@gmail.com>
2017-11-27 09:46:49 -0500

    print constructor

--- src/rpnpackage/Print.java
+++ src/rpnpackage/Print.java
@@ -7,6 +7,22 @@ package rpnpackage;
 public class Print implements Command {
 
 	private String command;
+	private Expression expression;
+
+	public Print(String line) {
+		command = PRINT;
+		/**
+		 * Split string in to array and get the expression
+		*/
+		String[] tempArr = line.split("\\s+");
+
+		/**
+		 * the expression will start at the second element
+		 * in the array
+		*/
+		String[] expArr = Arrays.copyOfRange(tempArr, 2, tempArr.length-1);
+		expression = new Expression(expArr);
+	}
 
 	public int evaluate(SymbolTable st) {
 
