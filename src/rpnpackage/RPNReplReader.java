78b60e57ecb1e848ad8677c8bd8cf7223df7aef6
Tyler Bobik <shed412@gmail.com>
2017-12-06 00:06:50 -0500

    did constructor

--- src/rpnpackage/RPNReplReader.java
+++ src/rpnpackage/RPNReplReader.java
@@ -1,7 +1,17 @@
 package rpnpackage;
 
+import java.io.*;
+
 public class RPNReplReader implements RPNReader {
 
+  private InputStreamReader stream;
+  private BufferedReader reader;
+
+  public RPNReplReader() {
+
+    stream = new InputStreamReader(System.in);
+    reader = new BufferedReader(stream);
+  }
 
   public String nextLine() {
     return null;
