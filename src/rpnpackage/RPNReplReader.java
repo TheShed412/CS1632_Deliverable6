555b9d788871da932197834ca96fc25a3eca1703
Tyler Bobik <shed412@gmail.com>
2017-12-06 00:32:25 -0500

    done with reader

--- src/rpnpackage/RPNReplReader.java
+++ src/rpnpackage/RPNReplReader.java
@@ -6,18 +6,31 @@ public class RPNReplReader implements RPNReader {
 
   private InputStreamReader stream;
   private BufferedReader reader;
+  private int lineNumber;
 
   public RPNReplReader() {
 
     stream = new InputStreamReader(System.in);
     reader = new BufferedReader(stream);
+    lineNumber = 0;
   }
 
   public String nextLine() {
-    return null;
+
+    System.out.print("> ");
+    lineNumber ++;
+    String expression = "";
+
+    try{
+      expression = reader.readLine();
+    } catch (Exception e) {
+      return null;
+    }
+
+    return expression;
   }
 
   public int getLineNumber() {
-    return -1;
+    return lineNumber;
   }
 }
