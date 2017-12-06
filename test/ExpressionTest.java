f09e8b47f3bae0beaafa6ef16bacb9480f6b6220
Tyler Bobik <shed412@gmail.com>
2017-12-05 22:12:09 -0500

    did big and small num test

--- test/ExpressionTest.java
+++ test/ExpressionTest.java
@@ -71,6 +71,41 @@ public class ExpressionTest {
 		assertEquals(actual, expected);
 	}
 
+	@Test
+	public void evaluateBigNumTest() throws Exception {
+		String rpnStr = "9999999999999999999999999 " +
+							"1234635373264729 +";
+		String expected = "10000000001234635373264728";
+
+		Expression exp = new Expression(rpnStr);
+
+		SymbolTable st = Mockito.mock(SymbolTable.class);
+
+		BigInteger actualBI = exp.evaluate(st);
+
+		String actual = actualBI.toString();
+
+		assertEquals(actual, expected);
+	}
+
+	@Test
+	public void evaluateSmallNumTest() throws Exception {
+		String rpnStr = "-9999999999999999999999999 " +
+							"9999999999999999999999999 +";
+		String expected = "0";
+
+		Expression exp = new Expression(rpnStr);
+
+		SymbolTable st = Mockito.mock(SymbolTable.class);
+
+		BigInteger actualBI = exp.evaluate(st);
+
+		String actual = actualBI.toString();
+
+		assertEquals(actual, expected);
+	}
+	
+
 
 	private int getInt(BigInteger bi) {
 		String str = bi.toString();
